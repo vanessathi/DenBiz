55d046f0a04a6788181d19977f18bd03d3d7d3c7
Tylre Collins <tyler@nhlp.com>
2020-05-06 14:03:46 -0600

    added full functionality to the index page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,17 @@
 const path = require('path');
+const db = require('../models');
+sequelize = require('sequelize');
 
 module.exports = function(app) {
+  // app.get('/', function(req, res) {
+  //   res.sendFile(path.join(__dirname, '../views/partials/index.html'));
+  // });
   app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../views/partials/index.html'));
+    db.smallBis.findAll({order: sequelize.random, limit: 1})
+        .then(function(data) {
+          const details = (data[0].dataValues);
+          res.render('index', details);
+        });
   });
   app.get('/registration', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/partials/registration.html'));
