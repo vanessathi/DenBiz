f7fd2b6af9f76f5ca49d37214166f46a079db44b
Tylre Collins <tyler@nhlp.com>
2020-05-06 11:18:49 -0600

    created post route and fixed error in server.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,40 +1,39 @@
 const db = require('../models');
 
 module.exports = function(app) {
-  app.get('/api/smallbusiness', function(req, res) {
-    db.smallbusiness.findAll({
-      include: [db.Post],
-    }).then(function(dbsmallbusiness) {
-      res.json(dbsmallbusiness);
-    });
-  });
+  // app.get('/api/smallbusiness', function(req, res) {
+  //   db.smallbusiness.findAll({
+  //     include: [db.Post],
+  //   }).then(function(dbsmallbusiness) {
+  //     res.json(dbsmallbusiness);
+  //   });
+  // });
 
-  app.get('/api/smallbusiness/:id', function(req, res) {
-    db.smallbusiness.findOne({
-      where: {
-        id: req.params.id,
-      },
-      include: [db.Post],
-    }).then(function(dbsmallbusiness) {
-      res.json(dbsmallbusiness);
-    });
-  });
-
+  // app.get('/api/smallbusiness/:id', function(req, res) {
+  //   db.smallbusiness.findOne({
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //     include: [db.Post],
+  //   }).then(function(dbsmallbusiness) {
+  //     res.json(dbsmallbusiness);
+  //   });
+  // });
   app.post('/api/smallbusiness', function(req, res) {
-    console.log(req.body)
+    console.log(req.body);
     db.smallBis.create(req.body).then(function(dbsmallbusiness) {
       res.json(dbsmallbusiness);
     });
   });
 
-  app.delete('api/smallbusiness/:id', function(req, res) {
-    db.smallbusiness.destroy({
-      where: {
-        id: req.params.id,
-      },
-    }).then(function(dbsmallbusiness) {
-      res.json(dbsmallbusiness);
-    });
-  });
+  // app.delete('api/smallbusiness/:id', function(req, res) {
+  //   db.smallbusiness.destroy({
+  //     where: {
+  //       id: req.params.id,
+  //     },
+  //   }).then(function(dbsmallbusiness) {
+  //     res.json(dbsmallbusiness);
+  //   });
+  // });
 };
 
