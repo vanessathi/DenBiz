7180ce27adcb779c015b6dace16dc69e393e412c
Tylre Collins <tyler@nhlp.com>
2020-05-14 10:44:38 -0600

    added compression package

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const db = require('./models');
+const compression = require('compression');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use(express.static('public'));
+app.use(compression());
 
 const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
